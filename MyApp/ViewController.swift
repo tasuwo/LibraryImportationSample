34526b581e822632f2ad15f1de469091521fd595
tasuwo <kamuhata.you@gmail.com>
2019-10-16 01:43:34 +0900

    :sparkles: Import and execute MyModule's function.

--- MyApp/ViewController.swift
+++ MyApp/ViewController.swift
@@ -7,12 +7,13 @@
 //
 
 import UIKit
+import MyModule
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        printMyName(nil)
     }
 
 
